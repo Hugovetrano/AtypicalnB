98ff78bcccfd2fcd24c2f118d4db0f2df83d974a
Hugo Petament <h.petament@icloud.com>
2023-06-19 14:43:04 +0200

    finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :properties
+  resources :bookings
+  get '/owner/properties', to: 'properties#index', as: :owner_properties
+  get '/owner/bookings', to: 'bookings#index', as: :owner_index
+  patch '/owner/bookings/:id', to: 'bookings#accept', as: :accept
+  patch '/owner/bookings/:id', to: 'bookings#refuse', as: :refuse
+  resources :users, only: %i[show]
 end

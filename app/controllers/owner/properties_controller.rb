a8565d854e85072960832d8223526e92348788a2
Hugo Petament <h.petament@icloud.com>
2023-06-20 22:02:08 +0200

    Implement Our experiences and My Reservations pages. Adding auto redirect when turn off owner mode when visiting owner pages. Minor fixs

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -1,4 +1,5 @@
 class Owner::PropertiesController < ApplicationController
+  before_action :check_owner_mode
   before_action :set_flat, only: [:show]
   def index
     @properties = Property.where(user_id: current_user.id)
@@ -11,5 +12,11 @@ class Owner::PropertiesController < ApplicationController
 
   def set_flat
     @property = Property.find(params[:id])
+  end
+
+  def check_owner_mode
+    unless current_user.owner_mode
+      redirect_to root_path
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+properties = Property.all
+
+properties.each do |p|
+    p.rating = rand(1..5)
+    p.save
+end

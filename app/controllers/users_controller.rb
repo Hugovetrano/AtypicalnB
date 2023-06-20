0c06694b1e207cefdcbdb91e482eec75665862dd
Hugo Petament <h.petament@icloud.com>
2023-06-20 18:28:40 +0200

    toggleOwner switch live !!!!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def toggleOwnerMode
+    if current_user.owner_mode == true
+      current_user.owner_mode = false
+    else
+      current_user.owner_mode = true
+    end
+    current_user.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -21,4 +22,6 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: %i[show]
+
+  get '/mode/toggle', to: 'users#toggleOwnerMode', as: :owner_mode_toggle
 end

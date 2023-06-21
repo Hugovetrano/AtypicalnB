cbae13e55bab61bad6a0fd05b7e0f19fbb6ac0fc
VincentBeaumont <beaumontvincent@yahoo.fr>
2023-06-21 23:59:43 +0200

    geocode_map

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,9 @@ class Property < ApplicationRecord
   has_many_attached :photos
   has_many :bookings
   belongs_to :user
+
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_address?
 end
 
 # array = []

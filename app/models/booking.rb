967f7fee84bb88a95bf46f4c3b5aa13171302db6
Hugo Vetrano <hugo.vetrano@ieseg.fr>
2023-06-22 17:51:52 +0200

    Navbar & start profile

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,7 @@ class Booking < ApplicationRecord
   has_many_attached :photos
   belongs_to :user
   belongs_to :property
+
+  # --------------------Validators-----------------------------------
+  validates :start_date, :end_date, presence: true
 end
